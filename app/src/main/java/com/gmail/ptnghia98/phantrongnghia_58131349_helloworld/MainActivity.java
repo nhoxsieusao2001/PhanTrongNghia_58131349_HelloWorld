c5da0cefd3fef88d532b8017b754316dcfb70973
nghia.phan@infodation.vn <ptnghia051098@gmail.com>
2020-04-11 12:03:40 +0700

    aa

---
+++ app/src/main/java/com/gmail/ptnghia98/phantrongnghia_58131349_helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.ptnghia98.phantrongnghia_58131349_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
